b4ff2190bd792e98eacd3747840d475fddbca92d
Farouk-Ben-Amor <faaroukbnamor@gmail.com>
2024-01-06 12:20:15 +0100

    App done (including bootstrap styling and dealing with services )

---
+++ src/app/reservation-list/reservation-list.component.ts
@@ -0,0 +1,28 @@
+import { Component,OnInit } from '@angular/core';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-reservation-list',
+  templateUrl: './reservation-list.component.html',
+  styleUrl: './reservation-list.component.css'
+})
+export class ReservationListComponent implements OnInit {
+  
+  reservations : Reservation[] = []
+
+  constructor(private reservationService : ReservationService,private router : Router){}
+  
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getReservations()
+  }
+  deleteReservation(id : string){
+
+    this.reservationService.deleteReservation(id)
+  }
+  // onSubmit(){
+  //   this.router.navigate(['/new'])
+  // }
+
+}
