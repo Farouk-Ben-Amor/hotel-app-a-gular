c93973d51ca1cf585b687faa3204697b239aae1a
Farouk-Ben-Amor <faaroukbnamor@gmail.com>
2024-01-06 18:41:32 +0100

    local storage removed

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -7,10 +7,7 @@ import { Reservation } from '../models/reservation';
 export class ReservationService {
   reservations : Reservation[] = [];
 
-  constructor(){
-    let savedReservations = localStorage.getItem("reservations")
-    this.reservations = savedReservations ? JSON.parse(savedReservations) : []
-  }
+  
 
   getReservations() : Reservation[] {
     return this.reservations
@@ -23,13 +20,11 @@ export class ReservationService {
   addReservation(reservation : Reservation) : void {
     reservation.id = Date.now().toString()
     this.reservations.push(reservation)
-    localStorage.setItem("reservations" , JSON.stringify(this.reservations))
   }
   
   deleteReservation(id : string) : void {
     let index = this.reservations.findIndex(res => res.id === id)
     this.reservations.splice(index,1)
-    localStorage.setItem("reservations" , JSON.stringify(this.reservations))
   }
   
   updateReservation(id:string ,updateReservation : Reservation) : void {
